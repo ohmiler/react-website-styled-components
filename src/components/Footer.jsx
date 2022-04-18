da130af1fd7d534bc1ee6a0986a22e2c200fbf56
Patiphan Phengpao <misterpatipan123@gmail.com>
2022-04-18 15:19:56 +0700

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FooterCon } from '../styles/styledElements'
+
+function Footer() {
+  return (
+    <FooterCon>
+        <p>&copy; 2022. ReactJS + Styled Components</p>
+    </FooterCon>
+  )
+}
+
+export default Footer
